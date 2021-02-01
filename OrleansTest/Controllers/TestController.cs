acb42b42dc50e8a5f6c939da4eb4d54145b4ba0a
Erik Zetterqvist <erik.zetterquist@trafikverket.se>
2021-02-01 15:27:07 +0100

    Add project files.

---
+++ OrleansTest/Controllers/TestController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Orleans;
+using OrleansTest.Grain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrleansTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly IClusterClient _clusterClient;
+
+        public TestController(IClusterClient clusterClient)
+        {
+            _clusterClient = clusterClient;
+        }
+
+        [HttpGet("{greeting}")]
+        public async Task<ActionResult<string>> Get(string greeting)
+        {
+            var grain = _clusterClient.GetGrain<IHello>(0);
+            var text = await grain.SayHello(greeting);
+            return new ActionResult<string>(text);
+        }
+    }
+}
---
+++ OrleansTest/Grain/Hello.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrleansTest.Grain
+{
+    public interface IHello : Orleans.IGrainWithIntegerKey
+    {
+        Task<string> SayHello(string greeting);
+    }
+    public class HelloGrain : Orleans.Grain, IHello
+    {
+        private readonly ILogger _logger;
+
+        public HelloGrain(ILogger<HelloGrain> logger)
+        {
+            this._logger = logger;
+        }
+
+        Task<string> IHello.SayHello(string greeting)
+        {
+            _logger.LogInformation($"\n SayHello message received: greeting = '{greeting}'");
+            return Task.FromResult($"\n Client said: '{greeting}', so HelloGrain says: Hello!");
+        }
+    }
+}
